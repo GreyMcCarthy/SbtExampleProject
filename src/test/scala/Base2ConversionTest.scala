adb62028359c4a9493d5eb5dde402579332bb58a
Grey McCarthy <greymcca@iu.edu>
2022-05-07 14:48:47 -0400

    Responded to review

--- src/test/scala/Base2ConversionTest.scala
+++ src/test/scala/Base2ConversionTest.scala
@@ -1,11 +1,12 @@
 import org.scalatest.funsuite.AnyFunSuite
 
-class Base2ConversionTest extends AnyFunSuite {
-  test("") {
-    assert(Base2Conversion.numDigits(600, 10) === 3)
+class BaseConversionTest extends AnyFunSuite {
+  test("BaseConversion on numDigits function") {
+    assert(BaseConversion.numDigits(600, 10) === 3)
   }
 
-  test("2") {
+
+  test("Base conversion test on numb digits for numbers 1 to 5000" ) {
     val listNumber= 1.to((5000))
     val listNumberTuple=listNumber.map { n =>
       val numDigits = n match {
@@ -19,13 +20,25 @@ class Base2ConversionTest extends AnyFunSuite {
     }
 
     listNumberTuple.foreach { case (x, base, numDigits) =>
-      assert(Base2Conversion.numDigits2(x, base) === numDigits, x)
+      assert(BaseConversion.numDigits2(x, base) === numDigits, x)
     }
   }
-  test("3"){
-  assert(Base2Conversion.baseConversion(2,2) === "10")
-  assert(Base2Conversion.baseConversion(50,4) === "302")
-    assert(Base2Conversion.baseConversion(500,15) === "235")
+  test("Base conversion test on intToVec function"){
+  assert(BaseConversion.intToVec(2,2) === "10")
+  assert(BaseConversion.intToVec(50,4) === "302")
+    assert(BaseConversion.intToVec(500,15) === "235")
   }
+  test("Base conversion test on vecToInt function for two examples"){
+    assert(BaseConversion.vecToInt(Vector(1,1,0,1),2 ) === 13)
+    assert(BaseConversion.vecToInt(Vector(4,4,4,1),5) === 621)
+  }
+  test("Convert int. to vec. then vec. to int. test") {
+    val listOfNum = 1.to(1000)
+    listOfNum.foreach { x =>
+      val xBase15 = BaseConversion.intToVec(x, 15)
+      assert(BaseConversion.vecToInt(xBase15, 15) === x)
+    }
+  }
+
 }
 
